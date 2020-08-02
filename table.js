4be3895c4a4454b54fa536207916c7d708473361
6110451410 <wichayada.h@ku.th>
2020-08-02 16:23:48 +0700

    edited timetable with JavaScript

---
+++ table.js
@@ -0,0 +1,161 @@
+/*MONDAY*/
+  function monday() {
+    var x = document.getElementById("onMon1");
+    var y = document.getElementById("onMon2");
+    if (x.style.display === "none" && y.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "none";
+      }
+  }
+  function monsub1() {
+    var x = document.getElementById("monsubject1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function monsub2() {
+    var x = document.getElementById("monsubject2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+/*TUESDAY*/
+  function tuesday() {
+    var x = document.getElementById("onTues1");
+    var y = document.getElementById("onTues2");
+    var z = document.getElementById("onTues3");
+    if (x.style.display === "none" && y.style.display === "none" && z.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "block";
+      z.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "none";
+        z.style.display = "none";
+      }
+  }
+  function tuessub1() {
+    var x = document.getElementById("tuessubject1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function tuessub2() {
+    var x = document.getElementById("tuessubject2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function tuessub3() {
+    var x = document.getElementById("tuessubject3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+/*WEDNESDAY*/
+  function wednesday() {
+    var x = document.getElementById("onWed1");
+    var y = document.getElementById("onWed2");
+    if (x.style.display === "none" && y.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "none";
+      }
+  }
+  function wedsub1() {
+    var x = document.getElementById("wedsubject1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function wedsub2() {
+    var x = document.getElementById("wedsubject2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+/*THURSDAY*/
+  function thursday() {
+    var x = document.getElementById("onThu1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "none";
+      }
+  }
+  function thusub1() {
+    var x = document.getElementById("thusubject1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+/*FRIDAY*/
+function friday() {
+    var x = document.getElementById("onFri1");
+    var y = document.getElementById("onFri2");
+    if (x.style.display === "none" && y.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "none";
+      }
+  }
+  function frisub1() {
+    var x = document.getElementById("frisubject1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function frisub2() {
+    var x = document.getElementById("frisubject2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+/*SATURDAY*/
+function saturday() {
+    var x = document.getElementById("onSat1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "block";
+    } else {
+        x.style.display = "none";
+        y.style.display = "none";
+      }
+  }
+  function satsub1() {
+    var x = document.getElementById("satsubject1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
